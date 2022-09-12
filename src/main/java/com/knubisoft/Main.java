285202c1dea2fbf046025015d09e63bc0cb9d292
Dmytro <dmytro.petrushenko@gmail.com>
2022-09-12 22:27:05 +0300

    add Person

--- src/main/java/com/knubisoft/Main.java
+++ src/main/java/com/knubisoft/Main.java
@@ -1,5 +1,7 @@
-package com.knubisoft;public class Main {
+package com.knubisoft;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
